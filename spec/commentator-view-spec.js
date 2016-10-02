92451403e1f45a5e4217c5adf8effcb7d43c2448
Danny Buonocore <danBuonocore@gmail.com>
2016-10-02 03:00:16 -0400

    Initial commit; set entry points for package

---
+++ spec/commentator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CommentatorView from '../lib/commentator-view';
+
+describe('CommentatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
